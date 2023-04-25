73607165acf11d4c1b9847e02e9913c06fee0c89
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-25 16:15:55 +0300

    Created the values state

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 type Props = {}
 
 const CreatePost = (props: Props) => {
+	const [values, Values] = useState({ title: '', body: '' })
+
 	return <div>CreatePost</div>
 }
 
