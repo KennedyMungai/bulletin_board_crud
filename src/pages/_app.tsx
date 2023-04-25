2aafba7b88beca75517ebd7b1b0d77adb5b7a2ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-25 14:02:54 +0300

    Added the theme prop to the ChakraProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import store from '@/app/store'
+import theme from '@/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
@@ -6,7 +7,7 @@ import { Provider } from 'react-redux'
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
-			<ChakraProvider>
+			<ChakraProvider theme={theme}>
 				<Component {...pageProps} />
 			</ChakraProvider>
 		</Provider>
