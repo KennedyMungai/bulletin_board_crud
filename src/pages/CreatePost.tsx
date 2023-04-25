8ae5beabecbb90bf6ca246a4a5f96872457f5ce3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-25 16:17:42 +0300

    Created an instance of useAppDispatch called dispatch

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,3 +1,4 @@
+import { useAppDispatch } from '@/app/hooks'
 import { useState } from 'react'
 
 type Props = {}
@@ -5,6 +6,8 @@ type Props = {}
 const CreatePost = (props: Props) => {
 	const [values, Values] = useState({ title: '', body: '' })
 	const [showPost, setshowPost] = useState(false)
+
+	const dispatch = useAppDispatch()
 
 	return <div>CreatePost</div>
 }
